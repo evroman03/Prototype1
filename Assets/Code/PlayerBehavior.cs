b18709207a7cf7e052d2c93f80aa097a9c87db9c
evroman03 <evroman@mail.bradley.edu>
2024-01-22 16:36:57 -0600

    Commit

--- Assets/Code/PlayerBehavior.cs
+++ Assets/Code/PlayerBehavior.cs
@@ -7,6 +7,8 @@ public class PlayerBehavior : MonoBehaviour
     private Controls controls;
     private Vector3 movement, speed;
     private float topSpeed;
+    private bool on = true;
+    public float Energy;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,30 +18,67 @@ public class PlayerBehavior : MonoBehaviour
         controls.ControllerMap.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
         controls.ControllerMap.Increase.performed += ctx => Increase();
         controls.ControllerMap.Decrease.performed += ctx => Decrease();
-        //controls.ControllerMap.Accelerate.performed += ctx => speed = ctx.ReadValue<Vector2>();
-       controls.ControllerMap.Accelerate.performed += ctx => Accelerate();
-       controls.ControllerMap.Decelerate.performed += ctx => Decelerate();
+        controls.ControllerMap.Accelerate.started += ctx => AccelerateOn();
+        controls.ControllerMap.Accelerate.canceled += ctx => AccelerateOff();
+        controls.ControllerMap.Decelerate.started += ctx => DecelerateOn();
+        controls.ControllerMap.Decelerate.canceled += ctx => DecelerateOff();
+        controls.ControllerMap.SelectSpeed.performed += ctx => SelectSpeed();
+        controls.ControllerMap.SelectAttack.performed += ctx => SelectAttack();
+        controls.ControllerMap.SelectShield.performed += ctx => SelectShield();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print(movement.x + ", " + movement.z);
-        print(speed);
+        if(on)
+        {
+            StartCoroutine(PrintSpeed());
+            on= false;
+        }
+        
+
+        //
+        //print(speed);
     }
-    void Accelerate()
+    IEnumerator PrintSpeed()
     {
-        print("+2");
+        print(movement.x + ", " + movement.z);
+        yield return new WaitForSeconds(.5f);
+        on = true;
     }
-    void Decelerate()
+    public void SelectSpeed()
     {
-        print("-2");
+        print("SPDSLCT");
     }
-    void Increase()
+    public void SelectAttack()
+    {
+        print("ATKSLCT");
+    }
+    public void SelectShield()
+    {
+        print("SHLDSLCT");
+    }
+    public void AccelerateOn()
+    {
+        print("ACCON");
+    }
+    public void AccelerateOff()
+    {
+        print("ACCOFF");
+    }
+    public void DecelerateOn()
+    {
+        print("DECON");
+    }
+    public void DecelerateOff()
+    {
+        print("DECOFF");
+    }
+    public void Increase()
     {
         print("+1");
     }
-    void Decrease()
+    public void Decrease()
     {
         print("-1");
     }
