22c7ec5e2c7de9ac75ccc47b26406625bba91ef8
evroman03 <evroman@mail.bradley.edu>
2024-01-22 16:04:59 -0600

    ProbuilderInstall

--- Assets/Code/PlayerBehavior.cs
+++ Assets/Code/PlayerBehavior.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     private Controls controls;
-    private Vector3 movement;
+    private Vector3 movement, speed;
+    private float topSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +16,25 @@ public class PlayerBehavior : MonoBehaviour
         controls.ControllerMap.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
         controls.ControllerMap.Increase.performed += ctx => Increase();
         controls.ControllerMap.Decrease.performed += ctx => Decrease();
+        //controls.ControllerMap.Accelerate.performed += ctx => speed = ctx.ReadValue<Vector2>();
+       controls.ControllerMap.Accelerate.performed += ctx => Accelerate();
+       controls.ControllerMap.Decelerate.performed += ctx => Decelerate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(movement.x + ", " + movement.z);
+        //print(movement.x + ", " + movement.z);
+        print(speed);
     }
-
+    void Accelerate()
+    {
+        print("+2");
+    }
+    void Decelerate()
+    {
+        print("-2");
+    }
     void Increase()
     {
         print("+1");
