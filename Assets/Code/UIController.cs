3eeb51315f6e8186c5cf06fd67dae2f36fae63fa
Fawful4Ever <114947591+Fawful4Ever@users.noreply.github.com>
2024-01-24 15:41:16 -0600

    UIControlller Updated
    UIController now has the code from both versions of the script

--- Assets/Code/UIController.cs
+++ Assets/Code/UIController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    public TMP_Text VariableText;
+    private int Speed = 1;
+    private int Attack = 1;
+    private int Shield = 1;
+    private int maxValue = 5;
+
     float energyheard; 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +35,51 @@ public class UIController : MonoBehaviour
     {
         PlayerBehavior.EnergyUpdated -= Handle_EnergyUpdated;
 
+    }
+
+    
+
+
+    public void ToggleUI(string variableType, bool increase)
+    {
+        if (variableType == "speed")
+        {
+            if (increase && Speed != maxValue)
+            {
+                ++Speed;
+                VariableText.text = "Speed = " + Speed;
+            }
+
+            else if (increase == false && Speed != 1)
+            {
+                --Speed;
+            }
+        }
+
+        else if (variableType == "attack")
+        {
+            if (increase && Attack != maxValue)
+            {
+                ++Attack;
+            }
+
+            else if (increase == false && Attack != 1)
+            {
+                --Attack;
+            }
+        }
+
+        else if (variableType == "shield")
+        {
+            if (increase && Shield != maxValue)
+            {
+                ++Shield;
+            }
+
+            else if (increase == false && Shield != 1)
+            {
+                --Shield;
+            }
+        }
     }
 }
