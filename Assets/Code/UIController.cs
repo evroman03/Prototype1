0a133a66e72674829c6aba57711c3c3a40b16cfd
kaptainKirby <rherwig@mail.bradley.edu>
2024-01-28 18:16:46 -0600

    Speedometer code created to match speed
    Current Calibration for speedometer is:
    0-237 inclusively.

--- Assets/Code/UIController.cs
+++ Assets/Code/UIController.cs
@@ -10,8 +10,9 @@ public class UIController : MonoBehaviour
     private bool isSelected = false;
     private float energyForUI=0, speedForUI=0, modifierSelected=0;
     private int speedMod = 1, shieldMod = 1, attackMod = 1;
+    private Vector3 rotationEuler;
 
-    [SerializeField] private GameObject spedomter;
+    [SerializeField] private GameObject speedomter;
     [SerializeField] private GameObject speedLevelUI;
     [SerializeField] private GameObject attackLevelUI;
     [SerializeField] private GameObject shieldLevelUI;
@@ -31,16 +32,26 @@ public class UIController : MonoBehaviour
         PlayerBehavior.SelectSpeed += UISelectSpeed;
         PlayerBehavior.SelectLeft += UISelectLeft;
         PlayerBehavior.SelectRight += UISelectRight;
+
+        //Sets needle of spedometer to starting position (180 degrees)
+        //spedomter.transform.GetChild(1).GetComponent<Image>().transform.eulerAngles = new Vector3(0, 0, -90);
     }
 
-    Vector3 rotationEuler;
-    Image needle;
+    //Max Angle to add is 147 degrees (from needle starting position)
+
+    float minAngle = 0;
+    float maxAngle = 237;
+    float angle = 0;
     void Update()
     {
         sliderObject.value = energyForUI;
 
-        rotationEuler += Vector3.forward * 30 * Time.deltaTime;
-        spedomter.transform.GetChild(1).GetComponent<Image>().transform.rotation = Quaternion.Euler(rotationEuler);
+        //Gets how much the needle needs to rotate
+        //rotationEuler += Vector3.back * 30 * Time.deltaTime;
+
+        //Changes needle rotation
+        angle = Mathf.Lerp(minAngle, maxAngle, Mathf.InverseLerp(minAngle, maxAngle, speedForUI));
+        speedomter.transform.GetChild(1).GetComponent<Image>().transform.eulerAngles = new Vector3(0, 0, -angle + 90);
     }
     public void Handle_EnergyUpdated(float energy)
     {
@@ -50,7 +61,7 @@ public class UIController : MonoBehaviour
     public void HandleSpeedUpdated(float speed)
     {
         speedForUI= speed;
-        //print("Current speed: " + speedForUI);
+        print("Current speed: " + speedForUI);
     }
     public void ChangeModifier(int posNeg)
     {
@@ -82,40 +93,57 @@ public class UIController : MonoBehaviour
         UpdateUI();
     }
 
+    /**
+     * Updates the UI components
+     */
     public void UpdateUI()
     {
+        //Gets which component to be modified
         switch (modifierSelected)
         {
+            //Speed Component
             case 0:
+                //Changes amount of Speed components to active color depending on the level
                 for (int i = 0; i < speedMod; i++)
                 {
                     speedLevelUI.transform.GetChild(i).GetComponent<Image>().color = speedUIColor;
                 }
+
+                //Sets the rest of the Speed components to gray
                 for (int i = speedMod; i < speedLevelUI.transform.childCount; i++)
                 {
                     speedLevelUI.transform.GetChild(i).GetComponent<Image>().color = Color.gray;
                 }
                 break;
+
+            //Shield Component
             case 1:
+                //Changes amount of Shield components to active color depending on the level
                 for (int i = 0; i < shieldMod; i++)
                 {
                     shieldLevelUI.transform.GetChild(i).GetComponent<Image>().color = shieldUIColor;
                 }
+
+                //Sets the rest of the Shield components to gray
                 for (int i = shieldMod; i < shieldLevelUI.transform.childCount; i++)
                 {
                     shieldLevelUI.transform.GetChild(i).GetComponent<Image>().color = Color.gray;
                 }
                 break;
             case 2:
+                //Changes amount of Attack components to active color depending on the level
                 for (int i = 0; i < attackMod; i++)
                 {
                     attackLevelUI.transform.GetChild(i).GetComponent<Image>().color = attackUIColor;
                 }
+
+                //Sets the rest of the Attack components to gray
                 for (int i = attackMod; i < attackLevelUI.transform.childCount; i++)
                 {
                     attackLevelUI.transform.GetChild(i).GetComponent<Image>().color = Color.gray;
                 }
                 break;
+            //If modifierSelected is none of the above
             default:
                 print("ERROR: UpdateUI() failed. Invalid modiferSelected.");
                 break;
