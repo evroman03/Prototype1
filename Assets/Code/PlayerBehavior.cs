813016ac8b0b70d0a3d8fbd3623e7cf0f774559c
Veizyl <sturner@mail.bradley.edu>
2024-02-12 15:49:52 -0600

    Found?

--- Assets/Code/PlayerBehavior.cs
+++ Assets/Code/PlayerBehavior.cs
@@ -184,7 +184,7 @@ public class PlayerBehavior : MonoBehaviour
         {
             if (wheel.AxleType == Axle.Front)
             {
-                var steerAngle = steerValue * Sensitivity * MaxSteerAngle; //* (1 - (CurrentSpeed / MaxSpeed));
+                var steerAngle = steerValue * Sensitivity * MaxSteerAngle * (1 - (CurrentSpeed / MaxSpeed));
                 var finalAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, steerAngle, 0.6f);
                 //Inverse relationship; as current speed increases, 25 will be multiplied by a smaller decimal to get a smaller angle. (To make it harder to accidently oversteer at high speed)       
                 //Lerp is included so that steering isn't instantanious
